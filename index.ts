3ffccfdba5d06fc95d8add290371ca96877276c3
Muhammad Asim <m.asim.rn@tectsoft.com>
2021-10-21 18:43:31 +0500

    Bulk user registration api created

--- index.ts
+++ index.ts
@@ -2,15 +2,22 @@ import express from 'express'
 // require('dotenv').config()
 import cors from 'cors'
 
+
+
+
 import { services } from './src/services/index'
 
 const app = express();
 
 // Middlewares
-
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
 // Mount REST on /api
 app.use(express.json())
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api', services);
 
 const port = 8000;
--- src/services/register/controller.ts
+++ src/services/register/controller.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+const readXlsxFile = require("read-excel-file/node");
+const excel = require("exceljs");
 var bcrypt = require('bcryptjs');
+var path = require("path");
 
 const prisma = new PrismaClient();
 
@@ -34,6 +37,62 @@ export const registerRouter = async (req: Request, res: Response, next: NextFunc
         return res.status(500).json({ message: 'something went wrong' })
       }
     }
+  }
+
+};
+
+export const registerBulkUserRouter = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (req.file == undefined) {
+      return res.status(400).send("Please upload an excel file!");
+    }
+    let realtivePath = path.resolve("uploads/" + req.file.filename);
+
+
+    readXlsxFile(realtivePath).then(async (rows: any) => {
+      // skip header
+      rows.shift();
+
+      let bulkUsers: any = [];
+
+      rows.forEach((row: any) => {
+        let users = {
+          user_name: row[0],
+          password: row[1],
+          email: row[2],
+          contact: row[3],
+          subscription_mode: row[4],
+        };
+        bulkUsers.push(users);
+      });
+
+      prisma.$transaction(
+        bulkUsers.map((data: any) => {
+          return prisma.user.upsert({
+            where: { email: data.email },
+            update: {},
+            create: {
+              user_name: data.user_name,
+              email: data.email,
+              contact: data.contact.toString(),
+              password: data.password,
+              subscription_mode: data.subscription_mode,
+            },
+          })
+        })
+      ).then(() => {
+        return res.status(200).json({})
+
+      }).catch((error) => {
+        return res.status(500).json({ message: 'Some fileds are missing in excel file' })
+      }
+      )
+
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Could not upload the file: " + req.file.originalname,
+    });
   }
 
 };
