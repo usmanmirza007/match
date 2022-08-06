1a0a4dbdfa7f0e7d6cf727650bf5377ab415cfd7
usmanmirza007 <33761314+usmanmirza007@users.noreply.github.com>
2022-08-06 19:18:55 +0500

    Update router.ts

--- src/services/auth/router.ts
+++ src/services/auth/router.ts
@@ -4,6 +4,6 @@ import * as controller from './controller';
 export const authRouter = express.Router();
 
 /** POST /api/auth */
-authRouter.route('/signUp').post(controller.registerRouter);
+authRouter.route('/signup').post(controller.registerRouter);
 authRouter.route('/login').post(controller.loginRouter);
 
